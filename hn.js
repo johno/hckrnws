7dd9607934d0dc964878f570a21274874c719dbe
John Otander <johnotander@gmail.com>
2013-12-03 22:40:40 -0700

    Add classes to article table elements.

--- hn.js
+++ hn.js
@@ -7,4 +7,11 @@ $(document).ready(function() {
   $('#header td').removeAttr('bgcolor');
   $('#header > td > table > tbody > tr > td:first-child').removeAttr('style').attr('id', 'img-wrapper');
   $('#header > td > table > tbody > tr > td:nth-child(2)').removeAttr('style').attr('id', 'title');
+
+  $('body > center > table > tbody > tr:nth-child(3)').attr('id', 'articles');
+
+  $('#articles > td > table tr:nth-child(3n-2)').addClass('article-title');
+  $('#articles > td > table tr:nth-child(3n-1)').addClass('article-details');
+  $('#articles > td > table tr:nth-child(3n)').remove();
+
 });
