fa0cf654748bd88f1a757f5e0d2214089866b858
John Otander <johnotander@gmail.com>
2014-07-08 10:19:05 -0600

    Simplify design enhancements.

--- hn.js
+++ hn.js
@@ -3,17 +3,9 @@
 $(document).ready(function() {
 
   $().makeHeaderPretty();
-
-  // Use url instead of this junk.
-  if($().isCommentsPage()) {
-    $().makeCommentsPagePretty();
-  } else if($().isAskPage()) {
-    // ...
-  } else {
-    $().makeArticlesPretty();
-  }
-
   $().makeFooterPretty();
+
+  $('*[bgcolor]').attr('bgcolor', '');
 });
 
 $.fn.makeHeaderPretty = function() {
@@ -26,60 +18,4 @@ $.fn.makeHeaderPretty = function() {
 
 $.fn.makeFooterPretty = function() {
   $('body > center > table > tbody > tr:last-child').attr('id', 'footer');
-}
-
-$.fn.makeArticlesPretty = function() {
-  $('body > center > table > tbody > tr:nth-child(3)').attr('id', 'articles');
-  $('body > center > table > tbody > tr:nth-child(2)').remove();
-  $().nukeArticlesBodyAndReplaceWithUl();
-}
-
-$.fn.nukeArticlesBodyAndReplaceWithUl = function() {
-  var articleTrs = $('#articles > td > table tr').toArray();
-  $('#articles').html("<ul id='articles-list'></ul>");
-
-  for(var i = 0; i < articleTrs.length; i += 3) {
-    $('#articles-list').append($().createLiFromTitleAndDetails(articleTrs[i], articleTrs[i+1]));
-  }
-  $('#articles-list').append(articleTrs[articleTrs.length-1]);
-  $('#articles-list > tr').attr('id', 'more').wrap('<li class="more-wrapper"></li>');
-}
-
-$.fn.createLiFromTitleAndDetails = function(currTitle, currDetails) {
-  if(!$(currTitle).find('.title').length) return;
-
-  return "<li>" +
-            $().getLinkFromTitle(currTitle) + '<br>' +
-            $().getPostDetails(currDetails)
-         "</li>";
-}
-
-$.fn.getLinkFromTitle = function(currTitle) {
-  return "<a class='title-link' href='" +
-         $(currTitle).find('.title a').attr('href') + "'>" +
-         $(currTitle).find('.title a').text() + "</a>"
-}
-
-$.fn.getPostDetails = function(currDetails) {
-  return "<span class='small'>" + $(currDetails).find('.subtext').html() + "</span>";
-}
-
-$.fn.makeCommentsPagePretty = function() {
-  $('body > center > table > tbody > tr:nth-child(2)').remove();
-
-  $('body > center > table > tbody > tr:nth-child(2)').attr('id', 'comment');
-  $('#comment > td > table:eq(0) > tbody > tr:eq(0)').addClass('article-title');
-  $('#comment > td > table:eq(0) > tbody > tr:nth-child(2)').addClass('article-details');
-  $('#comment > td > table:eq(0) > tbody > tr:nth-child(3)').remove();
-  $('#comment > td > table:eq(0) > tbody > tr:nth-child(3)').attr('id', 'add-comment-wrapper');
-  $('#comment > td > table:eq(1)').attr('id', 'comments-box-wrapper');
-  $('#comment > td > table:eq(1) > tbody > tr').addClass('comments-box');
-}
-
-$.fn.isCommentsPage = function() {
-  return $('body > center > table > tbody > tr:nth-child(3) > td > table tr:nth-child(4) td form').length > 0;
-}
-
-$.fn.isAskPage = function() {
-  return $('body > center > table > tbody > tr:nth-child(3) > td > table tr:nth-child(6) td form').length > 0;
 }
